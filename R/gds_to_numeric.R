1f306a1fdb577a835c1f501fec29602a4bb13b0b
Igor Kuivjogi Fernandes <igorkuivjogi@hotmail.com>
2024-03-01 13:38:12 -0600

    split vcf and gds functions

---
+++ R/gds_to_numeric.R
@@ -0,0 +1,47 @@
+gds_to_numeric <- function(file, code_as = "-101") {
+  genofile <- SNPRelate::snpgdsOpen(file, allow.duplicate = TRUE)
+  tab_geno <- SNPRelate::snpgdsGetGeno(genofile, snpfirstdim = TRUE, verbose = FALSE)  # 0,1,2
+  if (code_as == "-101") {
+    tab_geno <- tab_geno - 1
+  }
+  n <- nrow(tab_geno)
+  
+  child_nodes <- gdsfmt::ls.gdsn(genofile)
+  if (!("snp.rs.id" %in% child_nodes) & !("snp.id" %in% child_nodes)) {
+    stop("No SNP id information was found.")
+  }
+  
+  result <- data.frame(
+    snp.rs.id = rep(NA, n),
+    snp.id = rep(NA, n),
+    snp.allele = rep(NA, n),
+    snp.chromosome = rep(NA, n),
+    snp.position = rep(NA, n),
+    cm = rep(NA, n)
+  )
+  if ("snp.rs.id" %in% child_nodes) {
+    result$snp.rs.id <- gdsfmt::read.gdsn(gdsfmt::index.gdsn(genofile, "snp.rs.id"))
+  } 
+  if ("snp.id" %in% child_nodes) {
+    result$snp.id <- gdsfmt::read.gdsn(gdsfmt::index.gdsn(genofile, "snp.id"))  
+  }
+  if ("snp.allele" %in% child_nodes) {
+    result$snp.allele <- gdsfmt::read.gdsn(gdsfmt::index.gdsn(genofile, "snp.allele"))  
+  }
+  if ("snp.chromosome" %in% child_nodes) {
+    result$snp.chromosome <- gdsfmt::read.gdsn(gdsfmt::index.gdsn(genofile, "snp.chromosome"))  
+  }
+  if ("snp.position" %in% child_nodes) {
+    result$snp.position <- gdsfmt::read.gdsn(gdsfmt::index.gdsn(genofile, "snp.position"))  
+  }
+  colnames(result) <- c("snp.rs.id", "snp.id", "allele", "chr", "pos", "cm")
+  result <- cbind(result, tab_geno)
+  
+  # rename samples if they exist
+  if ("sample.id" %in% child_nodes) {
+    sample.id <- gdsfmt::read.gdsn(gdsfmt::index.gdsn(genofile, "sample.id"))
+    last_cols <- tail(colnames(result), ncol(tab_geno))
+    colnames(result)[colnames(result) %in% last_cols] <- sample.id
+  }
+  return(result)
+}
--- tests/testthat/test-gds_to_numeric.R
+++ tests/testthat/test-gds_to_numeric.R
@@ -1,20 +1,20 @@
-file <- system.file("test.vcf", package = "simplePHENOTYPES")
+file <- system.file("test.gds", package = "simplePHENOTYPES")
 meta_cols <- c("snp.rs.id", "snp.id", "allele", "chr", "pos", "cm")
 
 
 ##################
-# vcf_to_numeric #
+# gds_to_numeric #
 ##################
 
 test_that("code as -101 has expected possible values", {
-  result <- vcf_to_numeric(file, code_as = "-101")
+  result <- gds_to_numeric(file, code_as = "-101")
   geno <- result[!colnames(result) %in% meta_cols]
   possible_values <- sort(unique(unlist(as.vector(geno))))
   expect_equal(possible_values, c(-1, 0, 1))
 })
 
 test_that("code as 012 has expected possible values", {
-  result <- vcf_to_numeric(file, code_as = "012")
+  result <- gds_to_numeric(file, code_as = "012")
   geno <- result[!colnames(result) %in% meta_cols]
   possible_values <- sort(unique(unlist(as.vector(geno))))
   expect_equal(possible_values, c(0, 1, 2))
