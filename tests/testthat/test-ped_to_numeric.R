3769e80eedb4cc6ee8a136ac51d6fb9c481471f5
Igor Kuivjogi Fernandes <igorkuivjogi@hotmail.com>
2024-03-04 10:25:49 -0600

    add tests for bed and ped files

---
+++ tests/testthat/test-ped_to_numeric.R
@@ -0,0 +1,21 @@
+file <- system.file("test.ped", package = "simplePHENOTYPES")
+meta_cols <- c("snp.rs.id", "snp.id", "allele", "chr", "pos", "cm")
+
+
+##################
+# ped_to_numeric #
+##################
+
+test_that("code as -101 has expected possible values", {
+  result <- ped_to_numeric(file, code_as = "-101")
+  geno <- result[!colnames(result) %in% meta_cols]
+  possible_values <- sort(unique(unlist(as.vector(geno))))
+  expect_equal(possible_values, c(-1, 0, 1))
+})
+
+test_that("code as 012 has expected possible values", {
+  result <- ped_to_numeric(file, code_as = "012")
+  geno <- result[!colnames(result) %in% meta_cols]
+  possible_values <- sort(unique(unlist(as.vector(geno))))
+  expect_equal(possible_values, c(0, 1, 2))
+})
